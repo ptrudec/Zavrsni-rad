af4e5236690d644da660f73a3a674c8e9ec1445a
Petar <ptrudec@gmail.com>
2015-07-14 22:05:48 +0200

    izmjena da radi prikaz menija i na verijama prije lollipopa

--- app/src/main/java/tvz/zavrsni/eimenik/MainActivity.java
+++ app/src/main/java/tvz/zavrsni/eimenik/MainActivity.java
@@ -196,10 +196,10 @@ public class MainActivity extends AppCompatActivity /*ActionBarActivity*/ {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                /*R.drawable.ic_drawer, //nav menu toggle icon*/
-                R.string.app_name, // nav drawer open - description for accessibility
-                R.string.app_name // nav drawer close - description for accessibility
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.app_name,R.string.app_name
+                //R.drawable.ic_drawer, //nav menu toggle icon
+                //R.string.app_name, // nav drawer open - description for accessibility
+                //R.string.app_name // nav drawer close - description for accessibility
         ) {
             public void onDrawerClosed(View view) {
                 getSupportActionBar().setTitle(mTitle);
